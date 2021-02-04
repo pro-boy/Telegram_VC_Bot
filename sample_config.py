57094a4e465cde03a3ec2b2388345a9609177a52
PROBOY <71318599+pro-boy@users.noreply.github.com>
2021-02-04 11:41:45 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -1,4 +1,4 @@
-bot_token = "1488727769:AAElRU7yS309GP1t54F635ASDF"
+bot_token = "1637962147:AAEYROLyNUH8DIcCani9hSMGUsu2WsqAv7Y"
 radio_link = "http://peridot.streamguys.com:7150/Mirchi"
-sudo_chat_id = -1001485393652
-owner_id = 1243703097
+sudo_chat_id = -1001280048144
+owner_id = 1546367138
